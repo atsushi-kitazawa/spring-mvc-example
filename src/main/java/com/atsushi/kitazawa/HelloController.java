cad89b57dfac4d5ebafe64af004730c99be9c602
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-06-08 11:03:26 +0900

    fixed problem with data binding to jsp.

--- src/main/java/com/atsushi/kitazawa/HelloController.java
+++ src/main/java/com/atsushi/kitazawa/HelloController.java
@@ -1,14 +1,14 @@
 package com.atsushi.kitazawa;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String printHello(ModelMap model) {
+    public String printHello(Model model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
     }
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,4 +1,5 @@
-<%@ page contentType="text/html; charset = UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"%>
     <html>
 
     <head>
